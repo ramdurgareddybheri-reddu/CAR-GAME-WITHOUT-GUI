961cd5baeb136e2bbc0460761d8a745f0d79fe2d
RAMDURGAREDDY <ramdurgareddybheri@gmail.com>
2026-07-08 16:47:32 +0530

    UPDATE VERSION FOR IT

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-command=input('enter your command')
-command=('START STOP QUIT')
-i=command
-while command!='STOP QUIT':
-    command.upper()=="START"
-    print("THE CAR IS STARTED!")
-    if command.upper()=="STOP":
-        print("THE CAR IS STOPPED!")
-    elif command.upper()=="Quit":
-        print("THE CAR IS QUIT!")
+command=""
+command=input('>')
+while True:
 
+    if command.upper()=="START":
+        print("THE CAR IS STARTED")
+        break
+    elif command.upper()=="STOP":
+        print("THE CAR IS STOPPED!")
+        break
+    elif command.upper()=="RESTART":
+        print("THE CAR IS RESTARTED!")
+        break
+    elif command.upper() == "EXIT":
+        print("THE CAR IS EXITED!")
+        break
+    elif command.upper()=='HELP':
+        print("""" START THE CAR WITH START
+        STOP THE CAR WITH STOP
+        EXIT THE CAR WITH EXIT""")
+        print(input(">"))
+        break
 else:
-    print("THE CAR IS NOT STARTED!")
+    print("THE CAR IS NOT STARTED")
+    command = input("enter your command")
